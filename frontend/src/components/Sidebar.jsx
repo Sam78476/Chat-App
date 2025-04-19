38a336dfd9be7ed41b6121e1313ca8e9b9ca3b98
Sam <104291686+Sam78476@users.noreply.github.com>
2025-04-19 13:18:48 +0530

    Added Sidebar and light and dark themes

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./css/Sidebar.css";
+
+function Card(params) {
+    return (
+        <div className="card">
+            <img src={params.img} alt="Avatar" />
+            <div className="container">
+                <h3>{params.name}</h3>
+            </div>
+        </div>
+    );
+}
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+        
+        <div className="sidebar-header">
+            <h2>Chat App</h2>
+            <input type="text" placeholder="Search ..." />
+        </div>
+
+        <div className="sidebar-links">
+        <Card img="https://dummyimage.com/100x100/000/fff&text=User1" name="User 1" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User2" name="User 2" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+            <Card img="https://dummyimage.com/100x100/000/fff&text=User3" name="User 3" />
+        </div>
+
+        <div className="sidebar-footer">
+            <div className="user">
+                <img src="https://dummyimage.com/100x100/000/fff&text=User" alt="Avatar" />
+                <div className="user-info">
+                    <h3>User Name</h3>
+                    <p>Online</p>
+                </div>
+            </div>
+        </div>
+
+    </aside>
+  );
+}
+
+export default Sidebar;
